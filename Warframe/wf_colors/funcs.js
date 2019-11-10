df929b4300c299fbe503d550e270190445ffe06c
Gluckogon <57585934+Gluckogon@users.noreply.github.com>
2019-11-10 14:08:45 +0300

    Delete funcs.js

--- Warframe/wf_colors/funcs.js
+++
@@ -1,8 +0,0 @@
-function hex_to_rgb(hex) {
-    let int_ = parseInt(hex.slice(1), 16)
-    return [int_ >> 16, (int_ >> 8) & 255, int_ & 255]
-}
-
-function toggle_visibility(node) {
-    node.style.display = (node.style.display === 'none') ? 'block' : 'none'
-}
